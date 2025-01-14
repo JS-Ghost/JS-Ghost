ae5e36183fd9349596eb77f153510242f98e9f13
JS-Ghost <167707772+JS-Ghost@users.noreply.github.com>
2025-01-14 08:36:51 +0600

    first.c

--- first.c
+++ first.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
   int ID;
-  float n, count = 0;
+  float n, count = 0.0;
   printf("Enter your ID: ");
   scanf("%d", &ID);
   printf("Enter your number of unit: ");
@@ -10,20 +10,20 @@ int main(){
   if(n <= 50.0){
     count = n * 6.0;
   }
-  if(n > 50){
-    count = n * 8.5;
-  }
-  else if(n > 100.0){
-    count = n * 10.0;
-  }
-  if(count < 100){
-    printf("Your charge is: 100 TK\n");
-  }
-  else if(count > 500){
-    printf("Your charge is: %f\n",(count * 15.0)/100.0);
+  else if(n <= 100.0){
+    count = 300.0 + ((n - 50.0) * 8.5);
   }
   else{
-    printf("Your charge is: %f\n",count);
+    count = 300.0 + (50.0 * 8.5) + ((n - 100.0) * 10.0);
+  }
+  if(count < 100.0){
+    count  = 100.0;
+  }
+  if(count > 500){
+    count += 0.15;
+  }
+  else{
+    printf("Your charge is: %.2f TK\n",count);
   }
   return 0;
 }
